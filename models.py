3cb3f9b123227b20453942cf7e1fe3f09955d9be
Ching Fang <ching.fang@columbia.edu>
2021-03-04 10:31:30 -0500

    Add flexible cases for stay transitions

--- models.py
+++ models.py
@@ -7,6 +7,7 @@ class AnalyticSR(object):
         self.ca3 = modules.CA3(gamma, num_states)
         self.num_states = num_states
         self.gamma = gamma
+        self.estimates_T = False
 
     def forward(self, input, update=True):
         dg_out = self.dg.forward(input)
@@ -15,7 +16,7 @@ class AnalyticSR(object):
             self.update()
         return dg_out, ca3_out
 
-    def query(self, query_input, prev_input): # TODO
+    def query(self, query_input): # TODO
         return None, None
 
     def update(self):
@@ -27,6 +28,7 @@ class STDP_SR(object):
         self.ca3 = modules.STDP_Net(num_states)
         self.num_states = num_states
         self.gamma = gamma
+        self.estimates_T = True
 
     def forward(self, input, update=True):
         dg_out = self.dg.forward(input)
@@ -35,7 +37,7 @@ class STDP_SR(object):
             self.update()
         return dg_out, ca3_out
 
-    def query(self, query_input, prev_input): #TODO
+    def query(self, query_input): #TODO
         return None, None
 
     def update(self):
@@ -43,7 +45,32 @@ class STDP_SR(object):
         self.prev_input = input
 
 class STDP_LR_SR(STDP_SR):
-    def __init__(self, gamma, num_states):
+    def __init__(self, gamma, num_states, stay_case=0):
+        """
+        Args:
+            stay_case: 
+                0: run the model with no modifications
+                1: ignore stay inputs by stitching together transition inputs,
+                2: ignore stay inputs but continue learning rate update during stay
+        """
+
         super().__init__(gamma, num_states)
         self.ca3 = modules.STDP_LR_Net(num_states)
+        self.stay_case = stay_case
+
+    def forward(self, input, update=True):
+        is_stay = np.mean(self.ca3.prev_input == self.ca3.curr_input) == 1.0
+        if (not is_stay) or (self.stay_case == 0):
+            dg_out = self.dg.forward(input)
+            ca3_out = self.ca3.forward(input)
+            if update:
+                self.update()
+        elif self.stay_case == 1:
+            dg_out = None
+            ca3_out = None
+        else:
+            dg_out = None
+            ca3_out = None
+            self.ca3._decay_all_eta_invs()
+        return dg_out, ca3_out
 
