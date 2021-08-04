0a525f88416da36588b93550fed29122ebb5f412
Ching Fang <ching.fang@columbia.edu>
2021-08-04 15:53:06 -0400

    Clean up BPTT schedule + time tracking

--- train.py
+++ train.py
@@ -18,14 +18,13 @@ save_path = './trained_models'
 # Dataset Configs
 datasets = [inputs.Sim1DWalk]
 datasets_config_ranges = [{
-    #'num_steps': [3, 5, 15, 30],
-    'num_steps': [3, 10, 20, 40],
+    'num_steps': [3, 10, 15],
     'left_right_stay_prob': [[1, 1, 1], [7, 1, 1], [1, 4, 1]],
     'num_states': [5, 10, 15]
     }]
 num_datasets = len(datasets)
 p = {
-    'num_steps': [0.6, 0.4, 0., 0],
+    'num_steps': [0.8, 0.2, 0.0],
     }
 
 # Init net
@@ -40,12 +39,14 @@ optimizer = torch.optim.Adam(net.parameters(), lr=lr, weight_decay=weight_decay)
 running_loss = 0.0
 print_every_steps = 50
 train_steps = 50000
-time_task = 0
+time_step = 0
 time_net = 0
-
 grad_avg = 0
 
 for step in range(train_steps):
+    start_time = time.time()
+
+    # Select dataset parameters and load dataset
     dataset = datasets[step % num_datasets]
     dataset_config_rang = datasets_config_ranges[step % num_datasets]
     dataset_config = {}
@@ -54,36 +55,34 @@ for step in range(train_steps):
         p_key = p[key] if key in p else None
         sample_idx = np.random.choice(num_samples, p=p_key)
         dataset_config[key] = dataset_config_rang[key][sample_idx]
-    start_time = time.time()
     input = dataset(**dataset_config)
     dg_inputs = torch.from_numpy(input.dg_inputs.T).float().to(device).unsqueeze(1)
     dg_modes = torch.from_numpy(input.dg_modes.T).float().to(device).unsqueeze(1)
     net.ca3.set_num_states(input.num_states)
 
-    time_task += time.time() - start_time
-    start_time = time.time()
-
-    # zero the parameter gradients
+    # Zero grad and run network
     optimizer.zero_grad()
     _, outputs = net(dg_inputs, dg_modes, reset=True)
 
+    # Backprop the loss
     loss = criterion(
         net.ca3.get_T(),
         torch.tensor(net.ca3.get_real_T()).float()
         )
     loss = torch.sum(torch.sum(loss, dim=1))
     loss.backward()
-    #nn.utils.clip_grad_norm_(net.parameters(), 1)
-
+#    nn.utils.clip_grad_norm_(net.parameters(), 1)
     grad_avg += net.ca3.tau_pos.grad.item()
     optimizer.step()
 
-    time_net += time.time() - start_time
-
-    # print statistics
+    # Print statistics
+    elapsed_time = time.time() - start_time
+    time_step += elapsed_time
+    time_net += elapsed_time
     running_loss += loss.item()
 
     if step % print_every_steps == 0:
+        time_step /= print_every_steps
         if step > 0:
             running_loss /= print_every_steps
 
@@ -99,9 +98,7 @@ for step in range(train_steps):
 
         print('', flush=True)
         print('[{:5d}] loss: {:0.3f}'.format(step + 1, running_loss))
-        print('Time per step {:0.3f}ms'.format(1e3*(time_task+time_net)/(step+1)))
-        print('Total time on task {:0.3f}s, net {:0.3f}s'.format(time_task,
-                                                                 time_net))
+        print('Time per step {:0.3f}s, net {:0.3f}s'.format(time_step, time_net))
         print(f'A_pos: {net.ca3.A_pos.data.item()}')
         print(f'tau_pos: {net.ca3.tau_pos.data.item()}')
         print(f'A_neg: {net.ca3.A_neg.data.item()}')
@@ -112,19 +109,20 @@ for step in range(train_steps):
         print(f'Update activity clamp: {net.ca3.update_activity_clamp.x0.data.item()}')
         model_path = os.path.join(save_path, 'model.pt')
         torch.save(net.state_dict(), model_path)
+        time_step = 0
         running_loss = 0.0
         grad_avg = 0
 
-    if step == 30*print_every_steps:
+    if step == 2000:
         for key in p:
-            p[key] = [0.3, 0.6, 0.1, 0]
-    elif step == 50*print_every_steps:
+            p[key] = [0.7, 0.2, 0.1]
+    elif step == 4000:
         for key in p:
-            p[key] = [0.2, 0.3, 0.5, 0]
-    elif step == 80*print_every_steps:
+            p[key] = [0.6, 0.25, 0.15]
+    elif step == 6000:
         for key in p:
-            p[key] = [0, 0.4, 0.4, 0.2]
-    elif step == 100*print_every_steps:
+            p[key] = [0.5, 0.25, 0.25]
+    elif step == 7000:
         for key in p:
             p[key] = None
 
