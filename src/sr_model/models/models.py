99750c99de2184a3b45cf94ecdc45df1d87118bb
Ching <chingfang17@gmail.com>
2021-08-13 16:15:26 -0400

    Add linear and MLP model

--- src/sr_model/models/models.py
+++ src/sr_model/models/models.py
@@ -1,5 +1,6 @@
 import numpy as np
 import torch
+import torch.nn as nn
 
 from sr_model.models import module, dg, ca3
 
@@ -80,7 +81,7 @@ class STDP_SR(module.Module):
         self.estimates_T = True
         self.stay_case = stay_case
 
-    def forward(self, dg_inputs, dg_modes, reset=True):
+    def forward(self, dg_inputs, dg_modes=None, reset=True, update=True):
         """
         Args:
             dg_inputs: (steps, batch, states) one-hot inputs
@@ -93,14 +94,15 @@ class STDP_SR(module.Module):
             self.ca3.reset()
         for step in np.arange(num_steps):
             dg_input = dg_inputs[step, :, :]
-            dg_mode = dg_modes[step, :]
-            prev_dg_mode = dg_modes[step-1, :] if step != 0 else np.nan
+            if dg_modes is not None:
+                dg_mode = dg_modes[step, :]
+                prev_dg_mode = dg_modes[step-1, :] if step != 0 else np.nan
 
-            if dg_mode == 0: # Predictive mode
+            if dg_modes is None or dg_mode == 0: # Predictive mode
                 is_stay = step > 1 and torch.equal(self.ca3.prev_input, self.ca3.curr_input)
                 if (not is_stay) or (self.stay_case == 0):
-                    dg_out = self.dg(dg_input)
-                    ca3_out = self.ca3(dg_input)
+                    dg_out = self.dg(dg_input, update_transition=update)
+                    ca3_out = self.ca3(dg_input, update_transition=update)
                     self.update()
                 elif self.stay_case == 1:
                     dg_out = self.dg(dg_input, update_transition=False)
@@ -120,4 +122,56 @@ class STDP_SR(module.Module):
 
     def update(self):
         self.ca3.update()
+
+class Linear(module.Module):
+    def __init__(self, input_size):
+        super(Linear, self).__init__()
+        self.M = torch.zeros(1, input_size, input_size)
+
+    def forward(self, inputs, reset=False):
+        """
+        inputs is (steps, batch, states)
+        """
+
+        if reset:
+            self._reset_weights()
+        outputs = []
+        for input in inputs:
+            try:
+                input = input.unsqueeze(1)
+                output = torch.bmm(input, self.M)
+                output = output.squeeze(0)
+                #input = input.unsqueeze(2)
+                #output = torch.bmm(self.M, input)
+                #output = output.squeeze(2)
+            except:
+                import pdb; pdb.set_trace()
+            outputs.append(output)
+        outputs = torch.stack(outputs)
+        return outputs
+
+    def _init_weights(self):
+        torch.nn.init.zeros_(self.M)
+
+class MLP(module.Module):
+    def __init__(self, input_size, hidden_size):
+        """
+        Assumes input size is == output size
+        """
+
+        super(MLP, self).__init__()
+        self.f = nn.Sequential(
+            #nn.Linear(input_size, input_size, bias=False)
+            nn.Linear(input_size, hidden_size), nn.ReLU(),
+            nn.Linear(hidden_size, hidden_size), nn.ReLU(),
+            nn.Linear(hidden_size, input_size)
+            )
+
+    def forward(self, inputs, reset=False):
+        return self.f(inputs)
+
+    def _init_weights(self):
+        for layer in self.children():
+           if hasattr(layer, 'reset_parameters'):
+               layer.reset_parameters()
 
