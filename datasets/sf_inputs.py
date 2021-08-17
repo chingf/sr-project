4f66be26fb350d39fc677b67e6fc28e627a1b559
Ching <chingfang17@gmail.com>
2021-08-17 14:06:50 -0400

    Add linear mix option for input features

--- datasets/sf_inputs.py
+++ datasets/sf_inputs.py
@@ -12,43 +12,69 @@ except:
     warnings.warn("Emily's experimental data could not be loaded.")
 
 from sr_model.utils import pol2cart, downsample
+from datasets import inputs
 
-class Sim1DWalk(object):
+class Sim1DWalk(inputs.Sim1DWalk):
     """
     Simulates a walk in a 1D ring, where you can go left/right/stay
     """
 
     def __init__(
             self, num_steps, left_right_stay_prob=[1, 1, 1], num_states=16,
-            feature_dim=32
+            feature_dim=32, feature_vals=[0,1], feature_type='linear'
             ):
 
-        self.num_steps = num_steps
-        self.left_right_stay_prob = np.array(left_right_stay_prob)
-        self.left_right_stay_prob = self.left_right_stay_prob/np.sum(self.left_right_stay_prob)
-        self.num_states = num_states
+        super(Sim1DWalk, self).__init__(
+            num_states, left_right_stay_prob, num_states
+            )
         self.feature_dim = feature_dim
-        self.dg_inputs, self.dg_modes, self.xs, self.ys, self.zs = self._walk()
+        self.feature_type = feature_type
+
+        if feature_type == 'nhot':
+            self.feature_dim = self.feature_dim//self.num_states * num_states
+            self.dg_inputs = np.repeat(
+                self.dg_inputs, self.feature_dim//self.num_states, axis=0
+                )
+        elif feature_type == 'linear':
+            self.expansion_mat = np.random.choice(feature_vals, (feature_dim, num_states))
+            self.dg_inputs = [self.expansion_mat@x for x in self.dg_inputs.T]
+            self.dg_inputs = np.array(self.dg_inputs).T
+        else:
+            raise ValueError(f'Feature type {feature_type} is not an option.')
 
     def get_true_T(self):
-        true_T = np.zeros((self.num_states, self.num_states))
-        for i in range(self.num_states):
-            true_T[i, i-1] = self.left_right_stay_prob[0]
-            true_T[i, i] = self.left_right_stay_prob[1]
-            true_T[i, (i+1)%self.num_states] = self.left_right_stay_prob[2]
-        return true_T
+        raise NotImplementedError(
+            "Transition matrix is ill-defined for non one-hot features."
+            )
 
-    def _walk(self):
-        curr_state = 0
-        dg_inputs = np.zeros((self.num_states, self.num_steps))
-        dg_modes = np.zeros((self.num_steps))
-        xs = np.zeros(self.num_steps)
-        ys = np.zeros(self.num_steps)
-        zs = np.zeros(self.num_steps)
-        for step in np.arange(self.num_steps):
-            action = np.random.choice([-1,0,1], p=self.left_right_stay_prob)
-            curr_state = (curr_state + action)%self.num_states
-            ys[step] = curr_state
-            dg_inputs[curr_state, step] = 1
-        dg_inputs = np.repeat(dg_inputs, self.feature_dim//self.num_states, axis=0)
-        return dg_inputs, dg_modes, xs, ys, zs
+class Sim2DWalk(inputs.Sim2DWalk):
+    """
+    Simulates a 2D random walk around a 10x10 arena.
+    """
+
+    def __init__(
+            self, num_steps, num_states,
+            feature_dim, feature_vals=[0,1], feature_type='linear'
+            ):
+
+        super(Sim2DWalk, self).__init__(num_steps, num_states)
+        self.feature_dim = feature_dim
+        self.feature_type = feature_type
+
+        if feature_type == 'nhot':
+            self.feature_dim = self.feature_dim//self.num_states * num_states
+            self.dg_inputs = np.repeat(
+                self.dg_inputs, self.feature_dim//self.num_states, axis=0
+                )
+        elif feature_type == 'linear':
+            self.expansion_mat = np.random.choice(feature_vals, (feature_dim, num_states))
+            self.dg_inputs = [self.expansion_mat@x for x in self.dg_inputs.T]
+            self.dg_inputs = np.array(self.dg_inputs).T
+        else:
+            raise ValueError(f'Feature type {feature_type} is not an option.')
+
+    def get_true_T(self):
+        raise NotImplementedError(
+            "Transition matrix is ill-defined for non one-hot features."
+            )
+
