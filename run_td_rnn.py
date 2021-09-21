ea4d772f335d4e641436f118b30fbf5f0b04fb74
Ching <chingfang17@gmail.com>
2021-09-21 12:19:56 -0400

    Add option to not test over all transitions in run_td_rnn

--- run_td_rnn.py
+++ run_td_rnn.py
@@ -24,7 +24,7 @@ device = 'cpu'
 def run(
     save_path, net, dataset, dataset_config, print_file=None,
     print_every_steps=50, buffer_batch_size=32, buffer_size=5000, gamma=0.4,
-    train_net=False, return_dset=False, summ_write=True
+    train_net=False, return_dset=False, summ_write=True, test_over_all=True
     ):
 
     # Initializations
@@ -82,9 +82,14 @@ def run(
 
         # Calculate error
         with torch.no_grad():
-            all_transitions = buffer.memory
-            all_s = torch.stack([t[0] for t in all_transitions]).squeeze(1)
-            all_next_s = torch.stack([t[1] for t in all_transitions]).squeeze(1)
+            if test_over_all:
+                all_transitions = buffer.memory
+                all_s = torch.stack([t[0] for t in all_transitions]).squeeze(1)
+                all_next_s = torch.stack([t[1] for t in all_transitions]).squeeze(1)
+            else:
+                transitions = buffer.sample(min(step, buffer_batch_size))
+                all_s = torch.stack([t[0] for t in transitions], dim=2).squeeze(0)
+                all_next_s = torch.stack([t[1] for t in transitions], dim=2).squeeze(0)
             test_phi = all_s.squeeze(1)
             M = net.get_M(gamma)
             test_psi_s = torch.stack([s.squeeze() @ M for s in all_s])
