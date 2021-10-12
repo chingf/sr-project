877a1d7f6fb34d049fd642898af16616bca948e3
Ching <chingfang17@gmail.com>
2021-10-12 18:06:55 -0400

    Update TD-discrete-corr script to run over more smooth/precise parameter range

--- scripts/04_td_discrete_corr.py
+++ scripts/04_td_discrete_corr.py
@@ -20,87 +20,40 @@ def main(delete_dir=False):
         rmtree(save_path, ignore_errors=True)
 
     iters = 5
-    gammas = [0.4, 0.6, 0.8, 0.95]
-    spatial_sigmas = [1.0, 1.25, 1.5, 1.75, 2.0]
-    lr_range = [1E-2, 1E-3]
-    num_states = 100
-    num_steps = 2000
+    gammas = [0.7, 0.75, 0.8, 0.85]
+    spatial_sigmas = [0., 0.5, 1.0, 1.25, 1.5, 1.75, 2.0, 2.25]
+    sparsity_ps = [0.2, 0.15, 0.1, 0.075, 0.05, 0.03, 0.02, 0.01]
+    lr_range = [5E-3, 1E-3, 5E-4, 1E-4]
+    num_states = 20*20
+    num_steps = 6500
 
-    # Correlated, with 50/50 sampling
+    def grid_train(arg):
+        gamma, spatial_sigma, sparsity_p = arg
+        dataset = sf_inputs_discrete.Sim2DWalk
+        feature_maker_kwargs = {
+            'feature_dim': num_states, 'feature_type': 'correlated_distributed',
+            'feature_vals_p': [1-sparsity_p, sparsity_p],
+            'spatial_sigma': spatial_sigma
+            }
+        dataset_config = {
+            'num_steps': num_steps, 'feature_maker_kwargs': feature_maker_kwargs,
+            'num_states': num_states
+            }
+        input_size = num_states
+        dset_path = save_path + f'sparsity{sparsity_p}/sigma{spatial_sigma}/{gamma}/'
+        if os.path.isdir(dset_path + 'rnn_fixedlr_alpha'):
+            return
+        run_models(
+            dset_path, iters, lr_range, dataset, dataset_config, gamma,
+            input_size, save_outputs=True
+            )
+
+    args = []
     for gamma in gammas:
         for spatial_sigma in spatial_sigmas:
-            dataset = sf_inputs_discrete.Sim2DWalk
-            feature_maker_kwargs = {
-                'feature_dim': num_states, 'feature_type': 'correlated_distributed',
-                'spatial_sigma': spatial_sigma
-                }
-            dataset_config = {
-                'num_steps': num_steps, 'feature_maker_kwargs': feature_maker_kwargs,
-                'num_states': num_states
-                }
-            input_size = num_states
-            dset_path = save_path + f'pval50_sigma{spatial_sigma}/{gamma}/'
-            run_models(
-                dset_path, iters, lr_range, dataset, dataset_config, gamma,
-                input_size, save_outputs=True
-                )
-
-    # Correlated, with 80/20 sampling
-    for gamma in gammas:
-        for spatial_sigma in spatial_sigmas:
-            dataset = sf_inputs_discrete.Sim2DWalk
-            feature_maker_kwargs = {
-                'feature_dim': num_states, 'feature_type': 'correlated_distributed',
-                'feature_vals_p': [0.8, 0.2], 'spatial_sigma': spatial_sigma
-                }
-            dataset_config = {
-                'num_steps': num_steps, 'feature_maker_kwargs': feature_maker_kwargs,
-                'num_states': num_states
-                }
-            input_size = num_states
-            dset_path = save_path + f'pval80_sigma{spatial_sigma}/{gamma}/'
-            run_models(
-                dset_path, iters, lr_range, dataset, dataset_config, gamma,
-                input_size, save_outputs=True
-                )
-
-    # Correlated, with 95/5 sampling
-    for gamma in gammas:
-        for spatial_sigma in spatial_sigmas:
-            dataset = sf_inputs_discrete.Sim2DWalk
-            feature_maker_kwargs = {
-                'feature_dim': num_states, 'feature_type': 'correlated_distributed',
-                'feature_vals_p': [0.95, 0.05], 'spatial_sigma': spatial_sigma
-                }
-            dataset_config = {
-                'num_steps': num_steps, 'feature_maker_kwargs': feature_maker_kwargs,
-                'num_states': num_states
-                }
-            input_size = num_states
-            dset_path = save_path + f'pval95_sigma{spatial_sigma}/{gamma}/'
-            run_models(
-                dset_path, iters, lr_range, dataset, dataset_config, gamma,
-                input_size, save_outputs=True
-                )
-
-    # Correlated sparse
-    for gamma in gammas:
-        for spatial_sigma in spatial_sigmas:
-            dataset = sf_inputs_discrete.Sim2DWalk
-            feature_maker_kwargs = {
-                'feature_dim': num_states, 'feature_type': 'correlated_sparse',
-                'spatial_sigma': spatial_sigma
-                }
-            dataset_config = {
-                'num_steps': num_steps, 'feature_maker_kwargs': feature_maker_kwargs,
-                'num_states': num_states
-                }
-            input_size = num_states
-            dset_path = save_path + f'sparse_sigma{spatial_sigma}/{gamma}/'
-            run_models(
-                dset_path, iters, lr_range, dataset, dataset_config, gamma,
-                input_size, save_outputs=True
-                )
+            for sparsity_p in sparsity_ps:
+                args.append([gamma, spatial_sigma, sparsity_p])
+    Parallel(n_jobs=7)(delayed(grid_train)(arg) for arg in args)
 
 if __name__ == "__main__":
     main()
