504b45b3232fe247780964b936af7aa2ad56fafd
Ching <chingfang17@gmail.com>
2021-10-12 20:04:29 -0400

    Save exp statistics into a config file

---
+++ notebooks/configs.py
@@ -0,0 +1,12 @@
+payne2021_stats = {
+    'fieldsize': [0.072, 0.00678], # [mean, CI]
+    'nfields': [1.6747, 0.104], # [mean, CI]
+    'onefield': [0.5502] # [values,...]
+    }
+
+henrikson2010_stats = {
+    'fieldsize': [0.0725, 0.005], # [mean, CI]
+    'nfields': [1.6, 0.167], # [mean, CI]
+    'onefield': [0.57, 0.532] # [values,...]
+    }
+
