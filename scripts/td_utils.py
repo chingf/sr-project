49b45967e47909987f601901961443cfd7a6ef56
Ching <chingfang17@gmail.com>
2021-11-18 12:19:10 -0500

    Metalearn RNN-SF and have Hopfield optional in TD_utils

--- scripts/td_utils.py
+++ scripts/td_utils.py
@@ -18,61 +18,57 @@ from run_td_linear import run as run_linear
 
 def run_models(
     save_path, iters, lr_range, dataset, dataset_config, gamma, input_size,
-    save_outputs=False, test_over_all=True, print_file=None
+    save_outputs=False, test_over_all=True, print_file=None, run_hopfield=False
     ):
 
     # Hopfield
-    print(f'Running {save_path} for Hopfield')
-    net = Hopfield(input_size, lr=1E-3, clamp=np.inf)
-    for _iter in range(iters):
-        hopfield_save_path = save_path + f'hopfield/{_iter}'
-        if os.path.isfile(f'{hopfield_save_path}/results.p'):
-            print(f'{hopfield_save_path} already calculated. Skipping...')
-            continue
-        net.reset()
-        dset = dataset(**dataset_config)
-        dg_inputs = torch.from_numpy(dset.dg_inputs.T).float().to('cpu').unsqueeze(1)
-        outputs = net(dg_inputs)
-        if save_outputs:
-            results = {'outputs': outputs, 'dset': dset}
-            if not os.path.isdir(hopfield_save_path):
-                os.makedirs(hopfield_save_path)
-            with open(f'{hopfield_save_path}/results.p', 'wb') as f:
-                pickle.dump(results, f)
+    if run_hopfield: # Gamma doesn't matter for Hopfield so only run once
+        print(f'Running {save_path} for Hopfield')
+        net = Hopfield(input_size, lr=1E-3, clamp=np.inf)
+        for _iter in range(iters):
+            hopfield_save_path = save_path + f'hopfield/{_iter}'
+            if os.path.isfile(f'{hopfield_save_path}/results.p'):
+                print(f'{hopfield_save_path} already calculated. Skipping...')
+                continue
+            net.reset()
+            dset = dataset(**dataset_config)
+            dg_inputs = torch.from_numpy(dset.dg_inputs.T).float().to('cpu').unsqueeze(1)
+            outputs = net(dg_inputs)
+            if save_outputs:
+                results = {'outputs': outputs, 'dset': dset}
+                if not os.path.isdir(hopfield_save_path):
+                    os.makedirs(hopfield_save_path)
+                with open(f'{hopfield_save_path}/results.p', 'wb') as f:
+                    pickle.dump(results, f)
 
-    # Analytic RNN with fixed LR and alpha/beta scaling
+    # RNN-SF
     best_net = None; best_lr_val = np.inf;
     if os.path.isfile(save_path + f'rnn_fixedlr_alpha/{iters-1}/results.p'):
         print(f'{save_path}/rnn_fixed_lr_alpha already calculated. Skipping...')
         return
-    for lr in lr_range:
-        net = AnalyticSR(
-            num_states=input_size, gamma=gamma,
-            ca3_kwargs={'use_dynamic_lr':False, 'lr': lr}
-            )
-        train_net = False
-        try:
-            _, loss = run_rnn(
-                save_path + 'test/', net, dataset, dataset_config, gamma=gamma,
-                train_net=False, test_over_all=False
-                )
-        except RuntimeError as e:
-            if 'svd' in str(e):
-                continue
-            else:
-                raise
-        if loss < best_lr_val:
-            best_net = net; best_lr_val = loss;
+    num_iters = int(np.log(1E-5)/np.log(gamma))
+    ca3_kwargs = {
+        'use_dynamic_lr':False, 'parameterize': True,
+        'output_params':{'num_iterations': num_iters,'nonlinearity': 'clamp'}
+        }
+    net = AnalyticSR( # Initialize network
+        num_states=input_size, gamma=gamma,
+        ca3_kwargs=ca3_kwargs
+        )
+    _, _, _, net = run_rnn( # Meta-learn LR and nonlinearity
+        save_path + 'test/', net, dataset, dataset_config, gamma=gamma,
+        train_net=True, test_over_all=False
+        )
     for _iter in range(iters):
-        best_net.reset()
+        net.reset()
         rnn_save_path = save_path + f'rnn_fixedlr_alpha/{_iter}'
         if os.path.isfile(f'{rnn_save_path}/results.p'):
             print(f'{rnn_save_path} already calculated. Skipping...')
             continue
         try:
-            outputs, _, dset = run_rnn(
-                rnn_save_path, best_net, dataset, dataset_config, gamma=gamma,
-                return_dset=True, test_over_all=False
+            outputs, _, dset, _ = run_rnn(
+                rnn_save_path, net, dataset, dataset_config, gamma=gamma,
+                train_net=False, test_over_all=False
                 )
         except RuntimeError as e:
             if 'svd' in str(e):
