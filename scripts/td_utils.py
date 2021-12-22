1e6a2cfc4c3e603d71c27b6cff38359f502bd5dd
Ching <chingfang17@gmail.com>
2021-12-22 17:36:57 -0500

    Reorganize TD model scripts; no separate chance model needed

--- scripts/td_utils.py
+++ scripts/td_utils.py
@@ -21,96 +21,64 @@ def run_models(
     save_outputs=False, test_over_all=True, print_file=None, run_hopfield=False
     ):
 
-#    # Hopfield
-#    if run_hopfield: # Gamma doesn't matter for Hopfield so only run once
-#        print(f'Running {save_path} for Hopfield')
-#        net = Hopfield(input_size, lr=1E-3, clamp=np.inf)
-#        for _iter in range(iters):
-#            hopfield_save_path = save_path + f'hopfield/{_iter}'
-#            if os.path.isfile(f'{hopfield_save_path}/results.p'):
-#                print(f'{hopfield_save_path} already calculated. Skipping...')
-#                continue
-#            net.reset()
-#            dset = dataset(**dataset_config)
-#            dg_inputs = torch.from_numpy(dset.dg_inputs.T).float().to('cpu').unsqueeze(1)
-#            outputs = net(dg_inputs)
-#            if save_outputs:
-#                results = {'outputs': outputs, 'dset': dset}
-#                if not os.path.isdir(hopfield_save_path):
-#                    os.makedirs(hopfield_save_path)
-#                with open(f'{hopfield_save_path}/results.p', 'wb') as f:
-#                    pickle.dump(results, f)
-#
-#    # RNN-SF
-#    best_net = None; best_lr_val = np.inf;
-#    if os.path.isfile(save_path + f'rnn/{iters-1}/results.p'):
-#        print(f'{save_path}/rnn already calculated. Skipping...')
-#    else:
-#        num_iters = int(np.log(1E-5)/np.log(gamma))
-#        ca3_kwargs = {
-#            'use_dynamic_lr':False, 'parameterize': True,
-#            'output_params':{'num_iterations': num_iters,'nonlinearity': 'clamp'}
-#            }
-#        net = AnalyticSR( # Initialize network
-#            num_states=input_size, gamma=gamma,
-#            ca3_kwargs=ca3_kwargs
-#            )
-#        _, _, _, net = run_rnn( # Meta-learn LR and nonlinearity
-#            save_path + 'test/', net, dataset, dataset_config, gamma=gamma,
-#            train_net=True, test_over_all=False
-#            )
-#        for _iter in range(iters):
-#            net.reset()
-#            rnn_save_path = save_path + f'rnn/{_iter}'
-#            if os.path.isfile(f'{rnn_save_path}/results.p'):
-#                print(f'{rnn_save_path} already calculated. Skipping...')
-#                continue
-#            try:
-#                outputs, _, dset, _ = run_rnn(
-#                    rnn_save_path, net, dataset, dataset_config, gamma=gamma,
-#                    train_net=False, test_over_all=False
-#                    )
-#            except RuntimeError as e:
-#                if 'svd' in str(e):
-#                    continue
-#                else:
-#                    raise
-#            if save_outputs:
-#                results = {'outputs': outputs, 'dset': dset}
-#                with open(f'{rnn_save_path}/results.p', 'wb') as f:
-#                    pickle.dump(results, f)
-#
-#    # RNN-SF Shuffle
-#    if os.path.isfile(save_path + f'rnn_shuffle/{iters-1}/results.p'):
-#        print(f'{save_path}/rnn_shuffle already calculated. Skipping...')
-#    else:
-#        num_iters = int(np.log(1E-5)/np.log(gamma))
-#        ca3_kwargs = {
-#            'use_dynamic_lr':False, 'parameterize': True,
-#            'output_params':{'num_iterations': num_iters,'nonlinearity': 'clamp'}
-#            }
-#        net = AnalyticSR( # Initialize network
-#            num_states=input_size, gamma=gamma,
-#            ca3_kwargs=ca3_kwargs
-#            )
-#        for _iter in range(iters):
-#            rnn_save_path = save_path + f'rnn/{_iter}'
-#            rnn_shuffle_save_path = save_path + f'rnn_shuffle/{_iter}'
-#            model_path = f'{rnn_save_path}/model.pt'
-#            if not os.path.isfile(model_path):
-#                print(f'{rnn_save_path} not calculated. Skipping shuffle...')
-#                continue
-#            model_state_dict = torch.load(model_path)
-#            net.load_state_dict(model_state_dict)
-#            net.reset()
-#            outputs, _, dset, _ = run_rnn(
-#                rnn_shuffle_save_path, net, dataset, dataset_config, gamma=gamma,
-#                train_net=False, test_over_all=False, shuffle=True
-#                )
-#            if save_outputs:
-#                results = {'outputs': outputs, 'dset': dset}
-#                with open(f'{rnn_shuffle_save_path}/results.p', 'wb') as f:
-#                    pickle.dump(results, f)
+    # Hopfield
+    if run_hopfield: # Gamma doesn't matter for Hopfield so only run once
+        print(f'Running {save_path} for Hopfield')
+        net = Hopfield(input_size, lr=1E-3, clamp=np.inf)
+        for _iter in range(iters):
+            hopfield_save_path = save_path + f'hopfield/{_iter}'
+            if os.path.isfile(f'{hopfield_save_path}/results.p'):
+                print(f'{hopfield_save_path} already calculated. Skipping...')
+                continue
+            net.reset()
+            dset = dataset(**dataset_config)
+            dg_inputs = torch.from_numpy(dset.dg_inputs.T).float().to('cpu').unsqueeze(1)
+            outputs = net(dg_inputs)
+            if save_outputs:
+                results = {'outputs': outputs, 'dset': dset}
+                if not os.path.isdir(hopfield_save_path):
+                    os.makedirs(hopfield_save_path)
+                with open(f'{hopfield_save_path}/results.p', 'wb') as f:
+                    pickle.dump(results, f)
+
+    # RNN-SF
+    best_net = None; best_lr_val = np.inf;
+    if os.path.isfile(save_path + f'rnn/{iters-1}/results.p'):
+        print(f'{save_path}/rnn already calculated. Skipping...')
+    else:
+        num_iters = int(np.log(1E-5)/np.log(gamma))
+        ca3_kwargs = {
+            'use_dynamic_lr':False, 'parameterize': True,
+            'output_params':{'num_iterations': num_iters,'nonlinearity': 'clamp'}
+            }
+        net = AnalyticSR( # Initialize network
+            num_states=input_size, gamma=gamma,
+            ca3_kwargs=ca3_kwargs
+            )
+        _, _, _, net = run_rnn( # Meta-learn LR and nonlinearity
+            save_path + 'test/', net, dataset, dataset_config, gamma=gamma,
+            train_net=True, test_over_all=False
+            )
+        for _iter in range(iters):
+            net.reset()
+            rnn_save_path = save_path + f'rnn/{_iter}'
+            if os.path.isfile(f'{rnn_save_path}/results.p'):
+                print(f'{rnn_save_path} already calculated. Skipping...')
+                continue
+            try:
+                outputs, _, dset, _ = run_rnn(
+                    rnn_save_path, net, dataset, dataset_config, gamma=gamma,
+                    train_net=False, test_over_all=False
+                    )
+            except RuntimeError as e:
+                if 'svd' in str(e):
+                    continue
+                else:
+                    raise
+            if save_outputs:
+                results = {'outputs': outputs, 'dset': dset}
+                with open(f'{rnn_save_path}/results.p', 'wb') as f:
+                    pickle.dump(results, f)
 
     # RNN-SF Oja
     best_net = None; best_lr_val = np.inf;
@@ -152,42 +120,42 @@ def run_models(
                 with open(f'{rnn_save_path}/results.p', 'wb') as f:
                     pickle.dump(results, f)
 
-#    # Linear
-#    print(f'Running {save_path} for Linear')
-#    best_lr = np.inf; best_lr_val = np.inf;
-#    net = Linear(input_size=input_size)
-#    for lr in lr_range:
-#        net.reset()
-#        _, loss, _, _ = run_linear(
-#            save_path + 'test/', net, dataset, dataset_config, gamma=gamma, lr=lr,
-#            test_over_all=test_over_all, print_file=print_file
-#            )
-#        if loss < best_lr_val:
-#            best_lr = lr; best_lr_val = loss;
-#    for _iter in range(iters):
-#        net.reset()
-#        linear_save_path = save_path + f'linear/{_iter}'
-#        outputs, _, dset, _ = run_linear(
-#            linear_save_path, net, dataset, dataset_config, lr=best_lr, gamma=gamma,
-#            test_over_all=test_over_all, print_file=print_file
-#            )
-#        if save_outputs:
-#            results = {'outputs': outputs, 'dset': dset}
-#            with open(f'{linear_save_path}/results.p', 'wb') as f:
-#                pickle.dump(results, f)
-#   
-#    # MLP
-#    print(f'Running {save_path} for MLP')
-#    net = MLP(input_size=input_size, hidden_size=input_size*2)
-#    for _iter in range(iters):
-#        net.reset()
-#        mlp_save_path = save_path + f'mlp/{_iter}'
-#        outputs, _, dset,_ = run_mlp(
-#            mlp_save_path, net, dataset, dataset_config, gamma=gamma,
-#            test_over_all=test_over_all, print_file=print_file
-#            )
-#        if save_outputs:
-#            results = {'outputs': outputs, 'dset': dset}
-#            with open(f'{mlp_save_path}/results.p', 'wb') as f:
-#                pickle.dump(results, f)
-#
+    # Linear
+    print(f'Running {save_path} for Linear')
+    best_lr = np.inf; best_lr_val = np.inf;
+    net = Linear(input_size=input_size)
+    for lr in lr_range:
+        net.reset()
+        _, loss, _, _ = run_linear(
+            save_path + 'test/', net, dataset, dataset_config, gamma=gamma, lr=lr,
+            test_over_all=test_over_all, print_file=print_file
+            )
+        if loss < best_lr_val:
+            best_lr = lr; best_lr_val = loss;
+    for _iter in range(iters):
+        net.reset()
+        linear_save_path = save_path + f'linear/{_iter}'
+        outputs, _, dset, _ = run_linear(
+            linear_save_path, net, dataset, dataset_config, lr=best_lr, gamma=gamma,
+            test_over_all=test_over_all, print_file=print_file
+            )
+        if save_outputs:
+            results = {'outputs': outputs, 'dset': dset}
+            with open(f'{linear_save_path}/results.p', 'wb') as f:
+                pickle.dump(results, f)
+   
+    # MLP
+    print(f'Running {save_path} for MLP')
+    net = MLP(input_size=input_size, hidden_size=input_size*2)
+    for _iter in range(iters):
+        net.reset()
+        mlp_save_path = save_path + f'mlp/{_iter}'
+        outputs, _, dset,_ = run_mlp(
+            mlp_save_path, net, dataset, dataset_config, gamma=gamma,
+            test_over_all=test_over_all, print_file=print_file
+            )
+        if save_outputs:
+            results = {'outputs': outputs, 'dset': dset}
+            with open(f'{mlp_save_path}/results.p', 'wb') as f:
+                pickle.dump(results, f)
+
