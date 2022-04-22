3adfaeb65ae2efe004c368943d2cfcb299f56f23
Ching Fang <ching.fang@columbia.edu>
2022-04-22 14:47:16 +0000

    Update eigenval script to get max real eigenval of T

--- scripts/02_gamma_v_eigenvals.py
+++ scripts/02_gamma_v_eigenvals.py
@@ -14,56 +14,71 @@ import pickle
 experiment_dir = f'{configs.engram_dir}02_gamma_v_eigenvals/'
 experiment_dir = '../../engram/Ching/02_gamma_v_eigenvals/'
 os.makedirs(experiment_dir, exist_ok=True)
-n_jobs = 1
-n_iters = 10
+n_jobs = 56
+n_iters = 24
 
-num_steps = 3001
+num_steps = 1501
 num_states = 25
 dataset = inputs.Sim1DWalk
-dataset_params = {
-    'num_steps':num_steps, 'num_states':num_states,
-    'left_right_stay_prob':[4,1,1]
-    }
+lrs_probs = [[1,1,1], [4,1,1], [1,1,4]]
 
-gammas = [0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 0.95]
+gammas = np.arange(0.01, 0.86, 0.01)
+nonlinearity_args = [None, 1.0]
 args = []
 for gamma in gammas:
-    for _iter in range(n_iters):
-        args.append((gamma, _iter))
+    for nonlinearity_arg in nonlinearity_args:
+        for _iter in range(n_iters):
+            args.append((gamma, nonlinearity_arg, _iter))
 
 def grid(arg):
-    gamma, _iter = arg
+    gamma, nonlinearity_arg, _iter = arg
     eigenvals = []
     steps = []
-    output_params = {}
+    lrs_prob = lrs_probs[_iter%3]
+    dataset_params = {
+        'num_steps':num_steps, 'num_states':num_states,
+        'left_right_stay_prob': lrs_prob
+        }
+    dset = dataset(**dataset_params)
+    dg_inputs = torch.from_numpy(dset.dg_inputs.T).float().unsqueeze(1)
+    if nonlinearity_arg is None:
+        nonlin_str = 'Linear'
+        output_params = {}
+    else:
+        nonlin_str = 'Tanh'
+        rstep = int(np.log(1E-5)/np.log(gamma))
+        output_params = {
+            'num_iterations':rstep,
+            'nonlinearity': 'tanh', 'nonlinearity_args': nonlinearity_arg
+            }
     net_configs = {
         'gamma':gamma, 'ca3_kwargs':{'output_params': output_params}
         }
     net = STDP_SR(
         num_states=num_states, gamma=gamma, ca3_kwargs=net_configs['ca3_kwargs']
         )
-    dset = dataset(**dataset_params)
-    dg_inputs = torch.from_numpy(dset.dg_inputs.T).float().unsqueeze(1)
     for step in range(dset.num_steps):
         curr_dg_input = dg_inputs[step].unsqueeze(0)
         reset = True if step == 0 else False
         _, outputs = net(curr_dg_input, reset=reset)
         if step % 100 == 0:
-            M = net.get_M().detach().numpy()
-            eigenval, _ = np.linalg.eig(M)
-            max_eigenval = np.max(np.abs(eigenval))
-            eigenvals.append(max_eigenval)
+            T = net.get_T().detach().numpy()
+            eigenval, _ = np.linalg.eig(T)
+            max_eigenval = np.max(np.real(eigenval))
+            eigenvals.append(gamma*max_eigenval)
             steps.append(step)
-    return gamma, eigenvals, steps
+    return gamma, nonlin_str, eigenvals, steps
 
 results = {}
 results['gammas'] = []
+results['nonlinearity_args'] = []
 results['eigenvals'] = []
 results['steps'] = []
 job_results = Parallel(n_jobs=n_jobs)(delayed(grid)(arg) for arg in args)
 for res in job_results:
-    gamma, eigenvals, steps = res
+    gamma, nonlinearity_arg, eigenvals, steps = res
     results['gammas'].extend([gamma]*len(steps))
+    results['nonlinearity_args'].extend([nonlinearity_arg]*len(steps))
     results['eigenvals'].extend(eigenvals)
     results['steps'].extend(steps)
 with open(f'{experiment_dir}results.p', 'wb') as f:
